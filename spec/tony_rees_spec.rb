31ef3750259b55363e1c569914246d2cc3ef6d08
Dmitry Mozzherin <dmozzherin@gmail.com>
2009-07-21 10:25:50 -0400

    removed tonyrees spec

--- spec/tony_rees_spec.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby1.9
-# encoding: UTF-8
-
-require File.expand_path(File.dirname(__FILE__) + "/../lib/tony_rees")
-
-describe 'tony rees algorithm' do
-  mdld("Protozoa", "Pratozoa").should == 1
-  mdld("Protozoa", "Partozoa", 1).should == 2
-end
